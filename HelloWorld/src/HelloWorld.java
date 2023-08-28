b1374f5b237e7927af2c9421f72a642ddb90ae7f
brookemesinere <brookemesinere@localhost>
2023-08-28 14:14:16 -0400

    added goodbye to hello world

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Printout some statements
-	System.out.println("Hello welcome to mist 352");
+	System.out.println("Hello goodbye to mist 352");
 	System.out.println("Hello welcome to mist 352");
 	System.out.println("Hello welcome to mist 352");
 	System.out.println("Hello welcome to mist 352");
